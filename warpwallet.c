246d62abc96b71244fefa0897ac9ef9a3b46dd6b
RyanC <code@ryanc.org>
2015-08-22 21:35:38 -0700

    add warpwallet module

---
+++ warpwallet.c
@@ -0,0 +1,48 @@
+/* Copyright (c) 2015 Ryan Castellucci, All Rights Reserved */
+#include <time.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+// crypto.h used for the version
+#include <openssl/crypto.h>
+
+#include "libscrypt/libscrypt.h"
+
+#define _PBKDF2_i (1<<16)
+#define _SCRYPT_N (1<<18)
+#define _SCRYPT_r 8
+#define _SCRYPT_p 1
+
+#define openssl_pbkdf2(p, pl, s, ss, k, ks) \
+    PKCS5_PBKDF2_HMAC(p, pl, s, ss, _PBKDF2_i, EVP_sha256(), ks, k)
+
+#define scrypt(p, pl, s, ss, k, ks) \
+    libscrypt_scrypt(p, pl, s, ss, _SCRYPT_N, _SCRYPT_r, _SCRYPT_p, k, ks)
+
+int warpwallet(unsigned char *pass, size_t pass_sz,
+               unsigned char *salt, size_t salt_sz,
+               unsigned char *out) {
+    unsigned char seed1[32], seed2[32];
+
+    int i, ret, seed_sz = 32;
+
+    pass[pass_sz] = salt[salt_sz] = 1;
+    if ((ret = scrypt(pass, pass_sz+1, salt, salt_sz+1, seed1, seed_sz)) != 0) return ret;
+
+    pass[pass_sz] = salt[salt_sz] = 2;
+    openssl_pbkdf2(pass, pass_sz+1, salt, salt_sz+1, seed2, seed_sz);
+
+    // xor the scrypt and pbkdf2 output together
+    for (i = 0; i < 32; ++i) { out[i] = seed1[i] ^ seed2[i]; }
+
+    return 0;
+}
---
+++ warpwallet.h
@@ -0,0 +1,8 @@
+/*  Copyright (c) 2015 Ryan Castellucci, All Rights Reserved */
+#ifndef __BRAINFLAYER_WARPWALLET_H_
+#define __BRAINFLAYER_WARPWALLET_H_
+
+int warpwallet(unsigned char *, size_t, unsigned char *, size_t, unsigned char *);
+
+/* vim: set ts=2 sw=2 et ai si: */
+#endif /* __BRAINFLAYER_WARPWALLET_H_ */
