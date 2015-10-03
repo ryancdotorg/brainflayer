4e0741f872bf0a68e7be1a8baecdefcc62688b38
Guangyan Song <guangyan.song@gmail.com>
2015-10-04 00:20:33 +0100

    Create adbrk_main.cpp
    initial commit

---
+++ adbrk_main.cpp
@@ -0,0 +1,12 @@
+//(c) Nicolas Courtois, Guangyan Song, Ryan Castellucci 31 Sep 2015
+
+#include "ecc_k1_projects_basics.h" 
+
+void main(int argc, char * argv[])
+{
+	if (strstr(argv[1],"93000"))
+		My_Bitcoin_ECC_Testing93000(argv[2]);
+
+	Espace();
+};
+
