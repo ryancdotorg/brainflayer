7de53d63b24b206d97ee035288c8bd3159aa6096
RyanC <code@ryanc.org>
2017-10-06 06:48:34 -0700

    avoid generating inefficient ecm tables

--- ecmtabgen.c
+++ ecmtabgen.c
@@ -7,18 +7,53 @@
 #include <signal.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <math.h>
 
 #include <sys/stat.h>
 #include <sys/types.h>
 
 #include "ec_pubkey_fast.h"
 
+#define MIN_WINDOW (4)
+#define MAX_WINDOW (29)
+
 int main(int argc, char **argv) {
   int ret;
+  int req_bits, req_buckets;
+  int act_bits, act_buckets;
+
   if (argc != 3) {
     fprintf(stderr, "[!] Usage: %s window_size tablefile.tab\n", argv[0]);
     exit(1);
   }
+
+  req_bits = atoi(argv[1]);
+
+  if (req_bits < MIN_WINDOW) {
+    fprintf(stderr, "[!] Requested window_size %d too small, must be >= %d\n", req_bits, MIN_WINDOW);
+    exit(1);
+  }
+
+  if (req_bits > MAX_WINDOW) {
+    fprintf(stderr, "[!] Requested window_size %d too large, must be <= %d\n", req_bits, MAX_WINDOW);
+    exit(1);
+  }
+
+  // reduce window size if ineffeicent
+  act_bits = req_bits;
+  act_buckets = req_buckets = ceil(256.0/req_bits);
+  while (act_buckets == req_buckets) {
+    act_bits -= 1;
+    act_buckets = ceil(256.0/act_bits);
+  }
+  act_bits += 1;
+  act_buckets = ceil(256.0/act_bits);
+
+  if (act_bits < req_bits) {
+    fprintf(stderr, "[*] Requested window_size %d is inefficient, reduced to %d\n", req_bits, act_bits);
+  }
+
+  fprintf(stderr, "[*] Computing %d x 2^%d point multiplication matrix...\n", act_buckets, act_bits);
 
   if ((ret = secp256k1_ec_pubkey_precomp_table_save(atoi(argv[1]), argv[2])) < 0)
     fprintf(stderr, "[!] Failed to write tablefile '%s'\n", argv[2]);
