ddea113fff4a884a7c09e7912546d8676e6adb26
Ryan Castellucci <code@ryanc.org>
2020-11-22 11:30:15 +0000

    fine gcc, i'll inline this myself

--- blfchk.c
+++ blfchk.c
@@ -55,15 +55,36 @@ int main(int argc, char **argv) {
   }
 
   while (getline(&line, &line_sz, ifile) > 0) {
-    unhex(line, strlen(line), hash.uc, sizeof(hash.uc)); 
-    if (bloom_chk_hash160(bloom, hash.ul)) {
-      if (hfile && !hsearchf(hfile, &hash)) {
-        //fprintf(ofile, "%s (false positive)\n", hex(hash.uc, sizeof(hash.uc), buf, sizeof(buf)));
-        continue;
-      }
-      //fprintf(ofile, "%s\n", hex(hash.uc, sizeof(hash.uc), buf, sizeof(buf)));
-      fprintf(ofile, "%s", line);
+    unhex(line, strlen(line), hash.uc, sizeof(hash.uc));
+
+    unsigned int bit;
+    bit = BH00(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH01(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH02(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH03(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH04(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH05(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH06(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH07(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH08(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH09(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH10(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH11(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH12(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH13(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH14(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH15(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH16(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH17(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH18(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+    bit = BH19(hash.uc); if (BLOOM_GET_BIT(bit) == 0) { continue; }
+
+    if (hfile && !hsearchf(hfile, &hash)) {
+      //fprintf(ofile, "%s (false positive)\n", hex(hash.uc, sizeof(hash.uc), buf, sizeof(buf)));
+      continue;
     }
+    //fprintf(ofile, "%s\n", hex(hash.uc, sizeof(hash.uc), buf, sizeof(buf)));
+    fprintf(ofile, "%s", line);
   }
 
   return 0;
